4ec6cb7ef038704ece2e81e2bbed0a4965f6928a
rit894 <nagarithwikv@gmail.com>
2025-12-07 16:06:00 +0530

    upd.logic

--- Lab sessions/Text_Handling.py
+++ Lab sessions/Text_Handling.py
@@ -1,3 +1,29 @@
-with open ('FH.txt','r+') as f:
+'''with open ('FH.txt','r+') as f:
     data = f.read()
+    print(data)
+
+
+'''
+'''with open("FH.txt",'r') as f:
+    for line in f:
+        print(line.strip())
+with open ('FH.txt','r') as f:
+    print(f.read(12).strip())'''
+lines = ['python\n','file handling\n', 'prcticals\n']
+'''with open ('FH.txt','w') as f:
+    f.write('Hello world\n')'''
+'''with open('FH.txt', 'r') as f:
+    lines = f.readlines()
+
+with open('FH.txt', 'w') as f:
+    for line in lines:
+        f.write(line)
+        if line.strip() == 'prcticals':
+            f.write('found you\n')'''
+'''with open('FH.txt','a+') as f:
+    f.seek(0)
+    lines=f.readlines()
+    cleanlines=[line.strip() for line in lines]
+    print(cleanlines)'''
+
 
