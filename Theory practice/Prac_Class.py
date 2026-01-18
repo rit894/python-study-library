01babb023af70923ec98be6d4b48e9bcf1122791
rit894 <nagarithwikv@gmail.com>
2026-01-18 07:23:52 +0530

    Refactor Prac_Class.py: Update Dog and BankAccount classes, add Student and Book classes, and modify student initialization

--- Theory practice/Prac_Class.py
+++ Theory practice/Prac_Class.py
@@ -1,4 +1,4 @@
-class Dog:
+'''class Dog:
     species = "Canis familiaris"  # Class attribute
 
     def __init__(self, name, age):
@@ -19,8 +19,8 @@ print(Dog.name)        # ❌ Error
 dog1.species = "Wolf"  # Creates a new instance attribute
 print(dog1.species)    # "Wolf"
 print(dog2.species)    # "Canis familiaris"
-print(Dog.species)     # "Canis familiaris"
-class BankAccount:
+print(Dog.species)     # "Canis familiaris"'''
+'''class BankAccount:
     def __init__(self, name, amount):
         self.name = name
         self.amount = amount
@@ -41,8 +41,8 @@ class BankAccount:
 account1 = BankAccount("Alice", 1000)
 account1.display()          # Hi Alice, your current balance is ₹1000       
 account1.deposit(500)      # Alice deposited 500. New balance: 1500
-account1.withdraw(200)     # Alice withdrew 200. New balance: 1300
-
+account1.withdraw(200)     # Alice withdrew 200. New balance: 1300'''
+'''
 class student:
 
     def __init__(self,name,rollNo,DOB):
@@ -60,7 +60,6 @@ rollNo=input("Enter your roll number:")
 DOBY=int(input("Enter your date of birth year(YYYY):"))
 DOBM=int(input("Enter your date of birth month(MM):"))
 DOBD=int(input("Enter your date of birth day(DD):"))
-DOB=dt.datetime(DOBY,DOBM,DOBD)
 
 p_c=input('are you placed? (yes/no):').strip().lower()
 if p_c=='yes':
@@ -68,6 +67,30 @@ if p_c=='yes':
 else:
     position='not placed'
 
-s1=student(name,rollNo,DOB)
+s1=student(name,rollNo)
 s1.jobs(position)
-print(f"Name:{s1.name}\nRoll Number:{s1.rollNo}\nDate of Birth:{s1.DOB}\nPosition:{s1.postion}")
+print(f"Name:{s1.name}\nRoll Number:{s1.rollNo}\nDate of Birth:{s1.DOB}\nPosition:{s1.postion}")'''
+'''class Book:
+    def __init__(self,book, author):
+        self.book=book
+        self.author=author
+book=input("Enter book name:")
+author=input("Enter author name:")
+b1=Book(book,author)
+print(f"Book:{b1.book}\nAuthor:{b1.author}")'''
+
+class Student:
+    schoolName= 'School of computing'
+    def __init__(self,name,marks):
+        self.name=name
+        self.marks=marks
+    def display(self):
+        print(self.name)
+        print(self.marks)
+name=input()
+marks=int(input())
+s1=Student(name,marks)
+s1.display()
+        
+        
+
