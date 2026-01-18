dd356a68e2156121e084ba95d2a7f3c4fc00a46b
rit894 <nagarithwikv@gmail.com>
2026-01-18 09:57:04 +0530

    prac_Modules.py

---
+++ Theory practice/prac_Modules.py
@@ -0,0 +1,14 @@
+# import random
+# coins = random.choice(['Heads', 'Tails'])
+# print(coins)
+
+# # FROM  IN A PARTICULAR MODULE
+# from random import choice
+# import random
+# number= random. randint(1,10)
+# print(number)
+# from random import shuffle
+# cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+# shuffle(cards)
+# print(cards)
+import statistics
